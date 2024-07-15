917613461fcba7397f60be61dbe54c59c695e7ab
Nathan Sindelar <nathansindelar@macbook-pro-3.lan>
2024-07-15 13:27:35 -0500

    It works! But w/ mystery on algorithm array test

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -12,47 +12,119 @@ const oldPointStructure = {
   10: ['Q', 'Z']
 };
 
-function oldScrabbleScorer(word) {
+let newPointStructure = transform(oldPointStructure);
+
+newPointStructure[' '] = 0;
+
+let simpleStructure = {
+   1: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+};
+
+let vowelBonusStructure = {
+   1: ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'],
+   3: ['A', 'E', 'I', 'O', 'U']
+};
+
+/* function oldScrabbleScorer(word) {
 	word = word.toUpperCase();
 	let letterPoints = "";
- 
+   let total = 0;
 	for (let i = 0; i < word.length; i++) {
- 
 	  for (const pointValue in oldPointStructure) {
- 
 		 if (oldPointStructure[pointValue].includes(word[i])) {
-			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+			letterPoints += `Points for '${word[i]}': ${pointValue}\n`;
+         total += Number(pointValue);
 		 }
- 
 	  }
 	}
-	return letterPoints;
- }
+	return letterPoints, total;
+ } */
 
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   let word = input.question("Let's play some scrabble! Enter a word: ");
+   
+   return word;
 };
 
-let newPointStructure;
+let simpleScorer = function(word){
+   word = word.toUpperCase();
+   let letterPoints = '';
+   let total = 0;
+   for (let i = 0; i < word.length; i++){
+      for (const pointValue in simpleStructure) {
+         if (simpleStructure[pointValue].includes(word[i])) {
+            letterPoints += `Points for '${word[i]}': ${pointValue}\n`;
+            total += Number(pointValue);
+         }
+       }
+   }
+   return letterPoints, total;
+};
 
-let simpleScorer;
+let vowelBonusScorer = function(word){
+   word = word.toUpperCase();
+   let total = 0;
+   for (let i = 0; i < word.length; i++){
+      for (pointValue in vowelBonusStructure){
+         if (vowelBonusStructure[pointValue].includes(word[i])){
+            total += Number(pointValue);
+         }
+      }
+   }
+   return total;
+};
 
-let vowelBonusScorer;
+let scrabbleScorer = function(word){
+   word = word.toLowerCase();
+   let total = 0;
+   for (let i = 0; i < word.length; i++){
+      total += newPointStructure[word[i]];
+      }
+      return total;
+   }
 
-let scrabbleScorer;
+let simpleScore = {
+   name: "Simple Score",
+   description: "Each letter is worth 1 point.",
+   scoreFunction: simpleScorer
+}
 
-const scoringAlgorithms = [];
+let bonusVowels = {
+   name: "Bonus Vowels",
+   description: "Vowels are 3 pts, consonants are 1 pt.",
+   scoreFunction: vowelBonusScorer
+}
 
-function scorerPrompt() {}
+let scrabble = {
+   name: "Scrabble",
+   description: "The traditional scoring algorithm.",
+   scoreFunction: scrabbleScorer
+}
 
-function transform() {};
+const scoringAlgorithms = [simpleScore, bonusVowels, scrabble];
+
+function scorerPrompt(word) {
+   let scorer = input.question("Choose your scoring method.\n0 -- Simple: 1 point per letter \n1 -- Vowel bonus: 3 points for vowels \n2 -- Traditional: Uses Scrabble score system\nSelect number: ")
+   return scoringAlgorithms[scorer].scoreFunction(word);
+}
+
+function transform(oldPointStructure) {
+   let newObject = {};
+   for (let pointValue in oldPointStructure) {
+       for (let i = 0; i < oldPointStructure[pointValue].length; i++) {
+           newObject[oldPointStructure[pointValue][i].toLowerCase()] = Number(pointValue);
+       }
+   }
+   return newObject;
+}
 
 function runProgram() {
-   initialPrompt();
-   
+   let word = initialPrompt();
+   let score = scorerPrompt(word);
+   console.log(`Total score for ${word}: ${score}.`);
 }
 
 // Don't write any code below this line //
---
+++ transform-function.js
@@ -0,0 +1,22 @@
+const oldPointStructure = {
+    1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+    2: ['D', 'G'],
+    3: ['B', 'C', 'M', 'P'],
+    4: ['F', 'H', 'V', 'W', 'Y'],
+    5: ['K'],
+    8: ['J', 'X'],
+    10: ['Q', 'Z']
+  };
+
+  function transform(oldPointStructure) {
+    let newObject = {};
+    for (let item in oldPointStructure) {
+        for (let i = 0; i < oldPointStructure[item].length; i++) {
+            newObject[oldPointStructure[item][i].toLowerCase()] = Number(item);
+        }
+    }
+    return newObject;
+}
+
+let newObject = transform(oldPointStructure);
+console.log(newObject);
